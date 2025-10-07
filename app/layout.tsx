c2e0799e2ebfdd2c1609622e7179274229f604f3
Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>
2025-10-07 22:20:31 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600"],
+});
+
+export const metadata: Metadata = {
+  title: "TravelTrucks",
+  description:
+    "TravelTrucks offers easy camper rentals with fast booking and secure payments for your perfect road trip.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.variable}>
+        <TanStackProvider>{children}</TanStackProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import dynamic from "next/dynamic";
+
+const ReactQueryDevtools =
+  process.env.NODE_ENV === "development"
+    ? dynamic(
+        () =>
+          import("@tanstack/react-query-devtools").then(
+            (mod) => mod.ReactQueryDevtools
+          ),
+        { ssr: false }
+      )
+    : () => null;
+export default function TanStackProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [queryClient] = useState(() => new QueryClient({}));
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {/* Devtools можно добавлять только в режиме разработки */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
